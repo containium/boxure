ea0e2a3ab68d6e4c5c2c17c84d6c45a4eab09fdc
Arnout Roemers <github@company.romeinszoon.nl>
2013-08-16 19:33:58 +0200

    Clean up BoxureClassLoader a bit.

--- src-java/boxure/BoxureClassLoader.java
+++ src-java/boxure/BoxureClassLoader.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 public class BoxureClassLoader extends URLClassLoader {
 
   private final static String ISOLATE =
-       "clojure.lang.RT.*"
+       "clojure\\.lang\\.RT.*"
     + "|clojure\\.lang\\.Compiler.*"
     + "|clojure\\.lang\\.Namespace.*"
     + "|clojure\\.lang\\.Symbol.*"
@@ -25,28 +25,21 @@ public class BoxureClassLoader extends URLClassLoader {
     + "|clojure\\.main.*"
     + "|clojure\\.java.*";
 
+
   public BoxureClassLoader(final URL[] urls, final ClassLoader parent) {
     super(urls, parent);
   }
 
+
   protected Class<?> loadClass(final String name, final boolean resolve)
     throws ClassNotFoundException {
 
-    Class<?> clazz = super.findLoadedClass(name);
+    final Class<?> clazz = super.findLoadedClass(name);
+    if (clazz != null) return clazz;
 
-    if (clazz == null)
-      if (name.matches(ISOLATE)) {
-        System.out.println("[Boxure loading class "+ name +"]");
-        clazz = super.findClass(name);
-      } else {
-        clazz = super.loadClass(name, resolve);
-        if (clazz != null && resolve)
-          super.resolveClass(clazz);
-        if (clazz == null)
-          // If this is thrown, there is no Clojure runtime available in the classpath of the box.
-          throw new ClassNotFoundException("Could load class "+ name +" in isolation.");
-      }
-
-    return clazz;
+    if (name.matches(ISOLATE)) {
+        System.out.println("[Boxure loading class "+ name +" in isolation]");
+        return super.findClass(name);
+    } else return super.loadClass(name, resolve);
   }
 }
