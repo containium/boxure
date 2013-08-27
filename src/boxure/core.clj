e5f23aaae3ff0c99712079050293ec55afd32761
Arnout Roemers <github@company.romeinszoon.nl>
2013-08-27 11:54:14 +0200

    Add public 'jar-project' function.

--- src/boxure/core.clj
+++ src/boxure/core.clj
@@ -59,6 +59,11 @@
     (init-profiles (project-with-profiles @project) [:default])))
 
 
+(defn jar-project
+  [path]
+  (read-project-str (read-from-jar path "project.clj") path))
+
+
 ;;; Boxure implementation.
 
 (defn- eval-in-boxure
@@ -119,7 +124,7 @@
   here."
   [options parent-cl jar-path]
   (let [jar-path (resolve-file-path "." jar-path)
-        project (read-project-str (read-from-jar jar-path "project.clj") jar-path)
+        project (jar-project jar-path)
         dependencies (when (:resolve-dependencies options)
                        (map #(.getAbsolutePath %)
                             (resolve-dependencies :dependencies project)))
