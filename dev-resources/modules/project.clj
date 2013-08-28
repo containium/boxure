925cc1c5410fedb621e0e53c38a0ff073958eea9
Arnout Roemers <github@company.romeinszoon.nl>
2013-08-28 16:22:00 +0200

    Add test module to version control.

--- dev-resources/modules/project.clj
+++ dev-resources/modules/project.clj
@@ -3,11 +3,7 @@
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 (defproject module "1.0"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 ;; [org.clojars.tcrawley/clojure "1.6.0-clearthreadlocals"]
-                 ]
-  :exclusions [org.clojure/clojure]
-  :boxure {:start module.core/start
-           :stop module.core/stop
-           :ring-var module.core/app
-           :config {:module/connect "localhost:123"}})
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :exclusions [org.clojure/clojure])
+
+;;; Don't forget to execute `zip module.jar *` after updates in this file!
